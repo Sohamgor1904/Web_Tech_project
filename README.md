159253758c44abdce42aecd4381c0c04aa8b968c
Sohamgor1904 <sohamgor1510@gmail.com>
2026-04-09 23:31:03 +0530

    Emptying the repository

--- README.md
+++
@@ -1,9 +0,0 @@
-# Web_Tech_project
-<br>
-Contributors - Soham Gor (23BIT164)
-<br>
-Ansh Parmar (23BIT197)
-<br>
-Jainee Goyani (23BIT174)
-<br>
-Ayushi Maniar (23BIT189)
--- backend/app/routes/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.middleware.auth_middleware import get_current_user
-from app.models.schemas import UserResponse
-
-router = APIRouter()
-
-@router.get("/me", response_model=UserResponse)
-async def get_me(user: dict = Depends(get_current_user)):
-    return user
